a94b6f689df72a7ec8bb081785bdf3501344a326
ProjetoSine <55815061+irvson@users.noreply.github.com>
2023-02-22 11:25:31 -0300

    Tic tac toe(Jogo da velha)

---
+++ script.js
@@ -0,0 +1,131 @@
+//Initial Data
+//Dados Iniciais
+let square = {
+    a1:'',  b1:'',  c1:'',
+    a2:'',  b2:'',  c2:'',
+    a3:'',  b3:'',  c3:''
+        
+};
+
+let player ='' ;
+let warning = '' ;
+let playing = false ;
+
+reset();
+//Events
+//Eventos
+
+document.querySelector('.reset').addEventListener('click',reset);
+document.querySelectorAll('.item').forEach(item =>{
+    item.addEventListener('click', itemClick);
+})
+
+
+//Functions
+//Funções
+
+function itemClick(event){
+
+    let item = event.target.getAttribute('data-item');
+    
+    if(playing && square[item] === ''){
+        square[item] = player;
+        renderSquare();
+        togglePlayer();
+    }
+}
+
+function reset(){
+    warning= '';
+    let random = Math.floor(Math.random() * 2); // gera um numero aleatorio entre 0 e 1
+    
+    if(random === 0){
+        player = 'x'
+    }else{
+        player= 'o'
+    }
+    
+    for(let i in square){
+        square[i] = ''
+
+    }
+
+    playing = true;
+
+renderSquare();
+renderInfo();
+
+}
+
+function renderSquare(){
+for( let i in square){
+       let item = document.querySelector(`div[data-item=${i}]`)
+        item.innerHTML = square[i];
+    
+}
+
+checkGame();
+}
+
+function renderInfo(){
+document.querySelector('.vez').innerHTML = player;
+document.querySelector('.resultado').innerHTML = warning;
+}
+
+
+function togglePlayer(){
+    if(player === 'x'){
+        player = 'o'
+    }else{
+        player = 'x'        
+    }
+    renderInfo();
+}
+
+function checkGame(){
+    if(checkWinnerFor('x')) {
+        warning ='O "X" venceu';
+        playing=false;
+        
+    }else if(checkWinnerFor('o')) {
+        warning ='O "O" venceu';
+        playing=false;
+    }else if(isfull()){
+        warning ='Deu empate';
+        playing=false;
+    }
+}
+
+function checkWinnerFor(player){
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for(let w in pos){
+        let pArray = pos[w].split(',');
+let hasWon = pArray.every(option => square[option] === player);
+     
+if(hasWon){
+    return true;
+}
+    }
+    return false;
+}
+
+function isfull(){
+    for(let i in square){
+        if(square[i] === ''){
+            return false
+        }
+    }
+    return true;
+}
